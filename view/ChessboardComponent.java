2feb582616ff127a215e31b8f47835cf8d8fec1e
三金 <133667177+L3jstar@users.noreply.github.com>
2023-05-23 21:31:24 +0800

    Update ChessboardComponent.java

--- view/ChessboardComponent.java
+++ view/ChessboardComponent.java
@@ -108,12 +108,14 @@ public class ChessboardComponent extends JComponent {
         riverCell.add(new ChessboardPoint(4, 5));
         riverCell.add(new ChessboardPoint(5, 4));
         riverCell.add(new ChessboardPoint(5, 5));
+
         trapCell.add(new ChessboardPoint(8, 2));
         trapCell.add(new ChessboardPoint(8, 4));
         trapCell.add(new ChessboardPoint(7, 3));
         trapCell.add(new ChessboardPoint(0, 2));
         trapCell.add(new ChessboardPoint(0, 4));
         trapCell.add(new ChessboardPoint(1, 3));
+
         denCell.add(new ChessboardPoint(8, 3));
         denCell.add(new ChessboardPoint(0, 3));
 
